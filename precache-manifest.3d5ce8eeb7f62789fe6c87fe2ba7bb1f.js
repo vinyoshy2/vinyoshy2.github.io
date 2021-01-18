5e18fe428bd51690e8e6c9c37dc790e2060ffc0f
Vinay Koshy <vinyoshy2@gmail.com>
2021-01-18 16:57:55 -0600

    Updates

---
+++ precache-manifest.3d5ce8eeb7f62789fe6c87fe2ba7bb1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052f28c9d9a9c268692865539fd03006",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e5d39503d1443be516",
+    "url": "/static/css/main.287897f3.chunk.css"
+  },
+  {
+    "revision": "a602aab73ce0b9c30ecd",
+    "url": "/static/js/2.0324582c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0324582c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e5d39503d1443be516",
+    "url": "/static/js/main.206ac0c1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
