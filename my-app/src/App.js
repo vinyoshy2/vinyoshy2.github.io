e6acc1293d15ccb09abc7da357769416084d22bd
Vinay Koshy <vinyoshy2@gmail.com>
2021-01-18 23:07:53 -0600

    mostly done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import Sidebar from "./components/sidebar/sidebar.js";
 import MainContent from "./components/main-content/main-content.js";
 import './App.css';
---
+++ my-app/src/components/publications/publications.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import "./publications.css";
+import pubJSON from "../../data/publications.json";
+
+export default class PublicationsList extends React.Component {
+    render() {
+        return (
+	   <div class="pub-list">
+               {pubJSON.publications.map((pub, index) => 
+                   <div class="pub-entry">
+  		       <p class="num">{"[" + (index + 1) + "] "}</p>
+		       <Publication authors={pub.authors} title={pub.title} 
+		           conference={pub.conference} year={pub.year} filename={pub.filename}/>
+		   </div>
+	       )}
+	   </div>
+        );
+    }
+}
+
+class Publication extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+//	        {(index == this.props.authors.length - 1) ? 
+//			{item}
+//	        : (item == "Vinay Koshy") ?
+//	            <b>{item}</b> :
+        let auths = this.props.authors.map((item, index) => (
+	    <> 
+		{ (item == "Vinay Koshy") ?
+  	            <b>{item}</b> :
+		    <>{item}</>
+		}
+		{
+                    (index == this.props.authors.length - 1) ?
+		        <>. </> :
+			<>, </>
+		}
+	    </>
+        ));
+	console.log(auths);
+	return (
+            <div class="pub">
+    	        <p>
+		    <span>
+		        {auths}
+		    </span>
+		    <span>
+		        {this.props.title + ". "}
+		    </span>
+		    <span>
+		        {this.props.conference + " "} 
+		    </span>
+		    <span>
+		        {this.props.year}.
+		    </span>
+		</p>
+            </div>
+	);
+    }
+}
+
+
